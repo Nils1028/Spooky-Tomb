2f15d5c012941d184968b645e68a190355467998
Suchti18 <78668364+Suchti18@users.noreply.github.com>
2025-09-10 22:05:42 +0200

    Moved code and created first texts

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,13 @@
 const canvas = this.document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
+// Init
 window.addEventListener('load', function() {
     canvas.width = GAME.width;
     canvas.height = GAME.height;
 
     resizeCanvas();
+    window.addEventListener('resize', resizeCanvas);
     
     while(!resources.images.explorpheus.isLoaded)
     {
@@ -13,8 +15,6 @@ window.addEventListener('load', function() {
     }
     Player.draw(ctx);
 });
-
-window.addEventListener('resize', resizeCanvas);
 
 function resizeCanvas() {
     const margin = 20;
@@ -27,17 +27,12 @@ function resizeCanvas() {
     canvas.style.height = GAME.height * scale + "px";
 }
 
-window.addEventListener("keydown", (event) => {
-    if(event.code === "KeyW" || event.code === "ArrowUp") {
-        Player.setPos(Player.position.x, Player.position.y - 1);
-    } else if(event.code === "KeyA" || event.code === "ArrowLeft") {
-        Player.setPos(Player.position.x - 1, Player.position.y);
-    } else if(event.code === "KeyS" || event.code === "ArrowDown") {
-        Player.setPos(Player.position.x, Player.position.y + 1);
-    } else if(event.code === "KeyD" || event.code === "ArrowRight") {
-        Player.setPos(Player.position.x + 1, Player.position.y);
-    }
+// Game loop
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    Player.draw(ctx);
-})
+gameLoop = (timestamp) => {
+    console.log("Test");
+
+    requestAnimationFrame(this.gameLoop)
+}
+
+requestAnimationFrame(this.gameLoop)
--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -17,3 +17,18 @@ const Player = {
         }
     }
 }
+
+window.addEventListener("keydown", (event) => {
+    if(event.code === "KeyW" || event.code === "ArrowUp") {
+        Player.setPos(Player.position.x, Player.position.y - 1);
+    } else if(event.code === "KeyA" || event.code === "ArrowLeft") {
+        Player.setPos(Player.position.x - 1, Player.position.y);
+    } else if(event.code === "KeyS" || event.code === "ArrowDown") {
+        Player.setPos(Player.position.x, Player.position.y + 1);
+    } else if(event.code === "KeyD" || event.code === "ArrowRight") {
+        Player.setPos(Player.position.x + 1, Player.position.y);
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    Player.draw(ctx);
+})
